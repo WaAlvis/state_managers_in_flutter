79e317b675fdfb7c07f79be88b963be7e4059e9a
William Andres Alvis Palencia <alvis.wap@gmail.com>
2023-10-02 06:24:49 -0500

    agregando Provider para el la data de Gatos, y lints para mejoras del codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:state_manager_in_flutter/providers/cat_provider.dart';
 import 'package:state_manager_in_flutter/providers/counter_provider.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class AppStateProvider extends StatelessWidget {
+  const AppStateProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CounterProvider()),
+        ChangeNotifierProvider<CatProvider>(create: (_) => CatProvider(), lazy: false,)
+      ],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/providers/cat_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CatProvider extends ChangeNotifier {
+
+  CatProvider(){
+    print('Cat Provider inicializado');
+  }
+
+}
